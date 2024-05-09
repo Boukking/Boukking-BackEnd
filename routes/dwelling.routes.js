a76bd487ae1e9a0c0950da1fa241325b813dc6b8
Huguiz <hugo.maurin@hotmail.com>
2024-05-09 19:36:06 +0200

    POST Dwelling created

--- routes/dwelling.routes.js
+++ routes/dwelling.routes.js
@@ -14,16 +14,29 @@ router.get('/', (req, res) => {
     res.json({ toto: "toto" });
 });
 
+// GET /dwelling/:_id
+router.get('/:id', (req, res) => {
+    const dwellingId = req.params.id;
+    Dwelling.find()
+        .then((dwelling) => {
+            res.status(201).json(dwelling);
+        })
+        .catch(err => {
+            res.status(500).json({ message: "Internal Server Error" });
+        });
+});
+
 // POST /dwelling
-router.post("/", isAuthenticated, (req, res, next) => {
-    const {title, type, maxNbPerson, adress, city, zip, country} = req.body;
-    if (!title || !type || !maxNbPerson || !adress || !city || !zip || !country) return res.status(400).json({ message: "Fields are missing" });
-    if (isNaN(Number(maxNbPerson))) return res.status(400).json({ message: "Max. number of persons need to be a number" });
+router.post("/", isAuthenticated, (req, res) => {
+    const { title, type, maxPersonNumber, adress, city, zip, country } = req.body;
+    
+    if (!title || !type || !maxPersonNumber || !adress || !city || !zip || !country) return res.status(400).json({ message: "Fields are missing" });
+    if (isNaN(Number(maxPersonNumber))) return res.status(400).json({ message: "Max. number of persons need to be a number" });
     if (!["House", "Apartment", "Villa", "Condominium", "Townhouse", "Cottage", "Bungalow", "Duplex", "Penthouse", "Loft", "Mobile Home", "Mansion", "Studio Apartment", "Chalet", "Farmhouse"].includes(type)) return res.status(400).json({ message: "Type is invalid" });
     const author = req.payload._id;
-    Dwelling.create({title, type, maxNbPerson, adress, city, zip, country, author})
+    Dwelling.create({ title, type, maxPersonNumber, adress, city, zip, country, author })
         .then((createdDwelling) => {
-            res.status(201).json({ _id: createdDwelling._id, title: createdDwelling.title });
+            res.status(201).json({ _id: createdDwelling._id });
         })
         .catch(err => {
             res.status(500).json({ message: "Internal Server Error" });
@@ -31,12 +44,38 @@ router.post("/", isAuthenticated, (req, res, next) => {
 });
 
 // POST /dwelling/img
-router.post("/img", fileUploader.single("imageUrl"), isAuthenticated, (req, res, next) => {
-    if (!req.file) {
-        next(new Error("No file uploaded!"));
-        return;
-    }
+router.post("/img", isAuthenticated, fileUploader.single("imageUrl"), (req, res, next) => {
+    if (!req.file) return res.status(400).json({ message: "Error while uploading the image" });
     res.json({ fileUrl: req.file.path });
+});
+
+// PUT /dwelling/:id
+router.put("/:id", isAuthenticated, (req, res, next) => {
+    const dwellingId = req.params.id;
+    const { title, type, maxPersonNumber, adress, city, zip, country, image } = req.body;
+
+    if (maxPersonNumber && isNaN(Number(maxPersonNumber))) return res.status(400).json({ message: "Max. number of persons need to be a number" });
+    if (image && typeof image !== "object") return res.status(400).json({ message: "Images must be in a list" });
+    if (type && !["House", "Apartment", "Villa", "Condominium", "Townhouse", "Cottage", "Bungalow", "Duplex", "Penthouse", "Loft", "Mobile Home", "Mansion", "Studio Apartment", "Chalet", "Farmhouse"].includes(type)) return res.status(400).json({ message: "Type is invalid" });
+    
+    const modifiedDwelling = {};
+    if (title) modifiedDwelling.title = title;
+    if (type) modifiedDwelling.type = type;
+    if (maxPersonNumber) modifiedDwelling.maxPersonNumber = maxPersonNumber;
+    if (adress) modifiedDwelling.adress = adress;
+    if (city) modifiedDwelling.city = city;
+    if (zip) modifiedDwelling.zip = zip;
+    if (country) modifiedDwelling.country = country;
+    if (zip) modifiedDwelling.zip = zip;
+    if (image) modifiedDwelling.image = image;
+
+    Dwelling.findByIdAndUpdate(dwellingId, modifiedDwelling, { new: true })
+        .then((updatedDwelling) => {
+            res.status(204).json(updatedDwelling);
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Failed to update the dwelling" });
+        });
 });
 
 // export
