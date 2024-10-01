0bfc853cd7de47f92b253e712f6c2a84983d5c18
Huguiz <hugo.maurin@hotmail.com>
2024-10-01 22:42:08 +0200

    Fix CORS 2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 // express middleware
 app.use(express.json());
 app.use(cors({
-    origin: "https://boukking-backend.adaptable.app"
+    origin: "https://boukking.netlify.app"
 }));
 
 // route
